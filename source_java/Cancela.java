55e8126b338626f7c71fd7caa4ed8891aa109522
Ciro <ciromoraismedeiros@gmail.com>
2016-12-17 10:35:30 -0300

    Cancela implementada

--- source_java/Cancela.java
+++ source_java/Cancela.java
@@ -1,8 +1,20 @@
 import org.jcsp.lang.*;
 
 public class Cancela implements CSProcess {
-	private One2OneChannel abrir;
+	private final ChannelInput abrir;
+	private final ChannelInput fechar;	
+	
+	public Cancela(ChannelInput abrir, ChannelInput fechar) {
+		this.abrir  = abrir;
+		this.fechar = fechar;
+	}
+	
 	public void run(){
+		this.abrir.read();
+		System.out.println("Cancela.abrir");
+		
+		this.fechar.read();
+		System.out.println("Cancela.fechar");
 	}
 	
 }
--- source_java/Sistema.java
+++ source_java/Sistema.java
@@ -1,23 +1,34 @@
 import org.jcsp.lang.*;
 
-public class Sistema {
+public class Sistema implements CSProcess{
 	// channel abrir, fechar, tick
-	One2OneChannel abrir = Channel.one2one();	
-	One2OneChannel fechar = Channel.one2one();	
-	One2OneChannel tick = Channel.one2one();
-	
-	channel entrar:{1..MAX_CARROS}
-	channel sair:{1..MAX_CARROS}
-	channel passar:{1..MAX_CARROS}
-	channel pegar_ticket:{1..MAX_HORAS}
-	channel pagar_ticket:{1..MAX_HORAS}
-	channel ocupar:{1..QTD_VAGAS}
-	channel liberar:{1..QTD_VAGAS}
-	channel hora:{0..MAX_HORAS}
-	
+	private One2OneChannel abrir		= Channel.one2one();	
+	private One2OneChannel fechar		= Channel.one2one();	
+	private One2OneChannel entrar		= Channel.one2one();	
+	private One2OneChannel sair			= Channel.one2one();	
+	private One2OneChannel passar		= Channel.one2one();	
+	private One2OneChannel pegar_ticket	= Channel.one2one();	
+	private One2OneChannel pagar_ticket	= Channel.one2one();	
+	private One2OneChannel ocupar		= Channel.one2one();	
+	private One2OneChannel liberar		= Channel.one2one();	
 	
 	public static void main (String args[]) {
-		System.out.println("");
+		(new Sistema()).run();
+	}
+	
+	public void run(){
+		Cancela c = new Cancela(abrir.in(), fechar.in());
+		
+		CSProcess teste = new CSProcess() {
+			public void run(){
+				abrir.out().write(null);
+				fechar.out().write(null);
+			}
+		};
+		
+		(new Parallel(new CSProcess[]{c, teste})).run();
+		
+		System.out.println("terminou");
 	}
 }
 
